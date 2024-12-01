cab9a73471436ef09a1abaade6182b04612e330a
Draca Baugh-Flowers <66185683+Gradius64@users.noreply.github.com>
2024-12-01 14:25:32 -0600

    Add files via upload

---
+++ Module 12 challenge.js
@@ -0,0 +1,139 @@
+import React from 'react';
+
+
+import Navigation from '.Navigation';
+
+/// Header component
+
+const Header = () => {
+
+// footer component
+    
+function createFooter() {
+    const footer = document.createElement('footer');
+    footer.innerHTML =
+
+    document.body.appendChild(footer);
+}
+
+createFooter();
+
+
+const Footer = () => {
+    return 
+
+const footerStyle = {
+    backgroundColor: '#333',
+    color: '#fff',
+}
+
+const listStyle = {
+    listStyleType: 'none',
+    padding: 0
+};
+
+    
+
+const Navigation = ({ currentSection, setCurrentSection }) => {
+    const section = ['About Me', 'Portfolio', 'Contact'];
+
+    
+
+    
+
+    
+        
+
+    
+    
+
+        
+
+    
+
+
+  
+        
+
+    
+
+
+
+        
+    
+
+
+
+    
+    
+
+
+
+//// Navigation component
+document.getElementById('myButton').addEventListener('click', function() {
+    window.location.href = 'https://example.com';
+});
+
+
+
+
+
+
+    
+       
+
+        
+    
+
+       
+
+    
+
+        
+    
+
+   
+   
+
+        
+   
+   
+
+
+
+   
+   
+
+    
+       
+   
+
+
+
+    
+
+        
+   
+   
+    
+
+
+        
+
+   
+
+    
+    
+    
+    
+
+
+       
+
+
+            
+
+    
+    
+    
+}}}
